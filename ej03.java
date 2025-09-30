ee13e6309ad5da0bf9a4e8796127258cf6865fac
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-09-30 17:17:07 +0000

    ejercicio

---
+++ ej03.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ej03 {
+    public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("tu edad");
+    int edad = sc.nextInt();
+    }
+}
