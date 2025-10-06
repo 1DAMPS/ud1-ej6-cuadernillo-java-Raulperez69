e9a1de7cd09859629f2ee73880875f081a6f5b6e
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-06 18:15:08 +0000

    ej06

---
+++ ej06.java
@@ -0,0 +1,10 @@
+public class ej06{
+    public static void main(String[] args) {
+        String nombreUsuario = "Raúl";
+        int edadUsuario = 21;
+        String ciudadUsuario = "sevilla" ;
+        System.out.println(nombreUsuario);
+        System.out.println(edadUsuario);
+        System.out.println(ciudadUsuario);
+    }
+}
