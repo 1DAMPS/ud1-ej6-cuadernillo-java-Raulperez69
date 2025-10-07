ebad09d7856057f2182282ccbf5f1790ed1d44de
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-07 17:16:43 +0000

    ej16.java

---
+++ ej16.java
@@ -0,0 +1,7 @@
+public class ej16 {
+    public static void main(String[] args) {
+        short numero = 30000;
+        short resultado = (short) (numero * 10);
+        System.out.println("Resultado: " + resultado);
+    }
+}
