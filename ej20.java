ef21dc68b4148898a1d842f2f0e9977a2b827854
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-07 17:49:37 +0000

    ej20.java

---
+++ ej20.java
@@ -0,0 +1,12 @@
+/**
+ * @author raul
+ * @version 1
+ */
+public class ej20 {
+    public static void main(String[] args) {
+        int numero1 = 17;
+        int numero2 = 22;
+        int suma = numero1 + numero2;
+         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
