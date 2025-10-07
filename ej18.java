227f4edd3eac61c5fafddce8bc32753d8fb7a904
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-07 17:25:07 +0000

    ej18.java

---
+++ ej18.java
@@ -0,0 +1,8 @@
+public class ej18 {
+    public static void main(String[] args) {
+        int numero1 = 17;
+        int numero2 = 22;
+        int suma = numero1 + numero2;
+         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
