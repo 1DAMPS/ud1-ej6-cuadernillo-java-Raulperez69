81c8d5b10792881d83b964ee1ae3ca1589576286
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-07 17:29:55 +0000

    ej19.java

---
+++ ej19.java
@@ -0,0 +1,7 @@
+public class ej19 {
+    public static void main(String[] args) {
+         int radio = 5;
+         double area = 3.1416 * radio * radio;
+         System.out.println(area);
+    }
+}
