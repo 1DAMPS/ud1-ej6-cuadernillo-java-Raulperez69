ea004d9e28549788e519a7109e01e00337238146
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-07 17:13:42 +0000

    ej15.java

---
+++ ej15.java
@@ -0,0 +1,7 @@
+public class ej15 {
+    public static void main(String[] args) {
+         int numero = 2147483647;
+         numero = numero + 1;
+         System.out.println("El resultado es: " + numero);
+    }
+}
