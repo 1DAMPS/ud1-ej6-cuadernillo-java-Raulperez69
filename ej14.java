3855664a99e6e1b80b222d71f2b2d95a5013043e
Raulperez69 <raul.perez.villavieja.al@iespoligonosur.org>
2025-10-07 17:10:36 +0000

    ej14.java

---
+++ ej14.java
@@ -0,0 +1,6 @@
+public class ej14 {
+    public static void main(String[] args) {
+        final int DIAS_SEMANA = 5;
+        System.out.println("La semana tiene " + DIAS_SEMANA + " días.");
+    }
+}
